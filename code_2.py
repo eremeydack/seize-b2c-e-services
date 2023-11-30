c9367adb7229c1ae0f49d86be0f5f37828d21db0
eremeydack <139877570+eremeydack@users.noreply.github.com>
2023-11-30 16:18:36 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,30 @@
+        print(f"Random Number: {item}")
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def generate_random_data():
+    main()
+def main():
+import random
+
+
+    data = generate_random_data()
+    for item in data:
+if __name__ == "__main__":
+}
+}
+
+function main() {
+function generateRandomData() {
+    const data = generateRandomData();
+main();
+    }
+
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+    });
+        console.log("Random Number: " + item);
+    return data;
